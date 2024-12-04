059271f2ede9acace43e112472ecfa2bb0751b20
Carlos Marti <carlos199m1@gmail.com>
2024-12-04 15:43:22 -0500

    Storing of model parameters added to script

--- scripts/models/3_fit_multilinear_model.py
+++ scripts/models/3_fit_multilinear_model.py
@@ -40,13 +40,24 @@ def prepare_data(plant_data):
 if __name__ == "__main__":
     n_iter = 10000
     lr = 0.01
-    
+
     # Load raw data
     plant_data = pd.read_csv("data/plant_data.csv", index_col=0)
     train = np.load("results/train_idx.npy")
     X0 = np.ones((plant_data.shape[0], 1))
     X = get_saturated_basis(plant_data).values
     x1, x2, y, exposure = prepare_data(plant_data)
+
+    print("Training model on 100% of the data")
+    model = MultilinearModel()
+    model.set_data(x1, x2, y, exposure)
+    model.fit(n_iter=n_iter, lr=lr)
+    torch.save(model, "results/multilinear.pkl")
+
+    print("\tStoring model parameters")
+    params = model.get_params()
+    params["theta1"].to_csv("results/multilinear.theta1.csv")
+    params["theta2"].to_csv("results/multilinear.theta2.csv")
 
     print("Training model on 90% of the data")
     model = MultilinearModel()
@@ -55,12 +66,6 @@ if __name__ == "__main__":
     )
     model.fit(n_iter=n_iter, lr=lr)
     torch.save(model, "results/multilinear.train.pkl")
-
-    print("Training model on 100% of the data")
-    model = MultilinearModel()
-    model.set_data(x1, x2, y, exposure)
-    model.fit(n_iter=n_iter, lr=lr)
-    torch.save(model, "results/multilinear.pkl")
 
     history = pd.DataFrame({"loss": model.history})
     history.to_csv("results/multilinear.history.csv")
