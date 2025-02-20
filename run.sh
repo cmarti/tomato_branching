34610e0cc5a52a0d12b149875949c24b9d7709fe
Carlos Marti <carlos199m1@gmail.com>
2025-02-19 19:44:17 -0500

    scripts names updated

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 echo "= Data preparation and model fitting ="
 python scripts/models/1_prepare_data.py
 python scripts/models/2_fit_linear_models.py
-python scripts/models/3_fit_multilinear_model.py
+python scripts/models/3_fit_hierarchical_model.py
 python scripts/models/4_evaluate_models.py
 python scripts/models/5_report_results.py
 
